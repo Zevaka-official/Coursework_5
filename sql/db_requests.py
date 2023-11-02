59d5db6c15f153b107ef3b436195a1b7ac1e3e32
Adam <A.ergashev@internet.ru>
2023-11-02 21:35:16 +0300

    FIX: fixed

--- sql/db_requests.py
+++ sql/db_requests.py
@@ -1,4 +1,4 @@
-# создание таблиц в БД
+# Запрос на создание таблиц в БД
 create_tables = """
                 CREATE TABLE IF NOT EXISTS employers (
                 employer_id serial PRIMARY KEY,
@@ -16,7 +16,11 @@ create_tables = """
                 );
                 """
 
-# проверка на дубликаты
+# Запрос, где перед вставкой данных выполняется проверка на отсутствие дубликатов.
+# Для этого используется подзапрос WHERE NOT EXISTS.
+# Внутри подзапроса SELECT employer_name FROM employers WHERE employer_name = %s проверяет,
+# есть ли уже запись с таким же значением employer_name в таблице employers.
+# Если такая запись уже существует, то вставка не выполняется
 insert_to_employers = """INSERT INTO employers (employer_name) 
                          SELECT %s
                          WHERE NOT EXISTS (
@@ -25,13 +29,13 @@ insert_to_employers = """INSERT INTO employers (employer_name)
                          WHERE employer_name = %s)
                          """
 
-# вставка данных в таблицу с вакансиями
+# Запрос для вставки данных в таблицу с вакансиями, где employer_id берём из таблицы employers
 insert_to_vacancies = """INSERT INTO vacancies (employer_id, vacancy_title, vacancy_url, vacancy_area,
                          salary_from, salary_to)
                          VALUES (%s, %s, %s, %s, %s, %s)
                          """
 
-# вывод списка работодателей и количества вакансий, связанных с каждым из них, в алфавитном порядке
+# Запрос выводит список работодателей и количество вакансий, связанных с каждым из них, в алфавитном порядке
 get_all_employers_query = """
                           SELECT employers.employer_name, COUNT(*) from employers
                           JOIN vacancies USING (employer_id)
@@ -39,8 +43,8 @@ get_all_employers_query = """
                           ORDER BY employers.employer_name
                           """
 
-# вывод информации о работодателях, названиях вакансий, средней зарплате и URL вакансии,
-# отсортированных по названию работодателя
+# Запрос выводит информацию о работодателях, названиях вакансий, средней зарплате и URL вакансии,
+# отсортированную по названию работодателя
 get_all_vacancies_query = """
                           SELECT employers.employer_name, vacancies.vacancy_title,
                           (vacancies.salary_from + vacancies.salary_to) / 2 AS average_salary, vacancies.vacancy_url
@@ -49,12 +53,12 @@ get_all_vacancies_query = """
                           ORDER BY employers.employer_name;
                           """
 
-# вывод средней зарплаты
+# Запрос выводит среднюю зарплату в целых числах без запятой
 get_avg_salary_query = "SELECT CAST(AVG((salary_from+salary_to)/2) AS INT) FROM vacancies"
 
-# вывод информации о работодателях, названиях вакансий, средней зарплате и URL вакансии,
+# Запрос выводит информацию о работодателях, названиях вакансий, средней зарплате и URL вакансии,
 # только для тех вакансий, у которых средняя зарплата выше средней зарплаты всех вакансий.
-# сортировка по названию работодателя.
+# Результаты сортируются по названию работодателя.
 get_high_salary_query = """
                         SELECT employers.employer_name, vacancies.vacancy_title, 
                         (vacancies.salary_from + vacancies.salary_to) / 2, vacancies.vacancy_url 
